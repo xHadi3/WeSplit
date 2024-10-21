661279ba5dd270b1daf4d9a915a4055af51574ef
Hadi Alzayer <hadiessam9@gmail.com>
2024-10-21 18:24:46 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Hadi Al zayer on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
